6a134511163eac80e85284769b51caef2a89f09f
dievee <nex7440@gmail.com>
2016-05-01 17:47:55 +0300

    Added DI and mock

---
+++ ML.Domain/Entities/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML.Domain.Entities
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Mark { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ ML.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ML.Domain.Abstract;
+using ML.Domain.Entities;
+
+namespace ML.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        private IProductRepository repository;
+        public MovieController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Movies);
+        }
+
+    }
+}
