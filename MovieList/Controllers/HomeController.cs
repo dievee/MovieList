6132a32cac832657c13dd4700ee32abba42cc8a9
dievee <nex7440@gmail.com>
2016-07-16 18:21:14 +0300

    Added ability to search movies in imdb

---
+++ MovieList/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieList.Models;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace MovieList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(string movieTitle, string movieYear)  //SearchMovie smovie
+        {
+            String[] titleWords = movieTitle.Split(new Char[] { ' ', '.', ',', '!', '?', ':' });
+
+            string formattedTitle = null;
+            Movie movie = null;
+
+            foreach (string i in titleWords)
+            {
+                formattedTitle += "+" + i;
+            }
+
+            string url = "http://www.omdbapi.com/?t=" + formattedTitle + "&y=";
+
+            if (movieYear != null) url += movieYear;
+
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(url);
+
+                dynamic movieFullInfo = JsonConvert.DeserializeObject(json);
+
+                movie = new Movie
+                {
+                    Title = movieFullInfo.Title,
+                    Description = movieFullInfo.Plot,
+                    Poster = movieFullInfo.Poster,
+                    IMDBRating = movieFullInfo.imdbRating,
+                    EventDate = (DateTime)movieFullInfo.Released,
+                    IMDBLink = "http://www.imdb.com/title/" + movieFullInfo.imdbID + "/"
+                };
+            }
+
+
+                return View();
+        }
+        public string SearchInIMDB()
+        {
+            return "";
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add(Movie movie)
+        {
+            return View();
+        }
+    }
+}
---
+++ MovieList/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieList.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Poster { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IMDBRating { get; set; }
+        public DateTime EventDate { get; set; }
+        public string IMDBLink { get; set; }
+        public string Comment { get; set; }
+        public string Mark { get; set; }
+    }
+
+    public class SearchMovie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+    }
+}
